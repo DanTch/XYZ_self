f78eb01d51ef358b3b1718d061729b33b8549b3e
Danyalss <danyalsoltani87@gmail.com>
2025-09-07 22:32:11 +0330

    شیی

---
+++ config.py
@@ -0,0 +1,20 @@
+# تنظیمات اصلی ربات
+BOT_TOKEN = "7585784009:AAH3Qv-_qucK0Xp3eSbM3x-Kx2Q5K4bSnvs"
+ADMIN_ID = 6270301002  # آیدی عددی ادمین
+
+# مسیر فایل عکس استارت
+WELCOME_IMAGE_PATH = "welcome.jpg"
+
+# شماره کارت برای پرداخت
+CARD_NUMBER = "6219861905143557"
+CARD_OWNER = "داود سلطانی"
+
+# امتیازات مورد نیاز
+VIP_POINTS = 30
+RESELLER_POINTS = 200
+
+# پاداش‌ها
+NEW_USER_BONUS = 5
+REFERRAL_BONUS = 3
+VIP_REFERRAL_BONUS = 15
+RESELLER_REFERRAL_BONUS = 32
