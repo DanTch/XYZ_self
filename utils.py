b4f40e01ab154b6ee4e5c63da321781949d63112
Danyalss <danyalsoltani87@gmail.com>
2025-09-08 01:09:58 +0330

    شیی

--- utils.py
+++ utils.py
@@ -1,25 +1,24 @@
 from config import *
-from database import db  # اضافه شده
 
-def calculate_referral_bonus(user_id, base_points):
+def calculate_referral_bonus(user_id, base_points, db_instance):
     """محاسبه پاداش چندلایه برای دعوت‌کنندگان"""
     bonuses = {}
-    level1 = get_inviter(user_id)
+    level1 = get_inviter(user_id, db_instance)
     if level1:
         bonuses[level1] = base_points
-        level2 = get_inviter(level1)
+        level2 = get_inviter(level1, db_instance)
         if level2:
             bonuses[level2] = base_points // 5  # یک پنجم امتیاز
     return bonuses
 
-def get_inviter(user_id):
+def get_inviter(user_id, db_instance):
     """دریافت دعوت‌کننده مستقیم کاربر"""
-    user = db.get_user(user_id)
+    user = db_instance.get_user(user_id)
     if user:
         return user[5]  # invited_by
     return None
 
-def format_user_info(user_data, referrals_count):  # تغییر یافت
+def format_user_info(user_data, referrals_count):
     """فرمت‌دهی اطلاعات کاربر"""
     return f"""
 👤 نام: {user_data[1]} {user_data[2]}
